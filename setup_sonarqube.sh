e82390791fa14b53ba6a37b92f13d922f1cbf1bc
Shubham Pawar <shubham.d.pawar.3333@gmail.com>
2025-01-23 23:59:52 +0530

    Update setup_sonarqube.sh

--- setup_sonarqube.sh
+++ setup_sonarqube.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 # Add Docker's official GPG key:
-
-# Update package index
-sudo apt-get update -y
-
-# Install required packages
-sudo apt-get install -y ca-certificates curl
-
-# Create keyrings directory and download Docker's GPG key
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources
+# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-# Update package index again after adding the repository
-sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
